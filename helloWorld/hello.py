eba273fd5afa1381e42810b2ee57278523d03850
Anastasia Coda <nastiuka@gmail.com>
2012-10-25 18:15:02 +0300

    IA NE ZNAIUUUU

--- helloWorld/hello.py
+++ helloWorld/hello.py
@@ -1,5 +1,9 @@
 
-eteration = 10 
-
-for x in range(0eteration):
+"""eteration = 10 
+for x in range(0,eteration):
 	print "Hello World" 
+"""
+for x % 3 == 0  in range (20):
+	print 'Sava'
+for x % 5 == 0 in range (20):
+	print 'Commit'
