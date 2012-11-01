ef869176dd992481d0ecabb93cd9b7a9006bcab2
Anastasia Coda <nastiuka@gmail.com>
2012-11-01 12:45:06 +0200

    tot corect ))))

--- helloWorld/below 1000.py
+++ helloWorld/below 1000.py
@@ -1,6 +1,6 @@
 num = 1000
-for x in range(0, num):
-	if x % 3 == 0 or x % 5 == 0:
-		x=x+1
-		suma = x
-		print suma
+suma = 0 
+for i in range(0, num):
+	if i % 3 == 0 or i % 5 == 0:
+		suma = suma + i 
+print suma		
