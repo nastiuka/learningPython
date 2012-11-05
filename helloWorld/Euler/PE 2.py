8dcf0aefcd3a56b908acc59c51dc28340d4e9cc0
Anastasia Coda <nastiuka@gmail.com>
2012-11-05 19:38:29 +0200

    PE 2

--- helloWorld/Euler/PE 2.py
+++ helloWorld/Euler/PE 2.py
@@ -3,9 +3,16 @@ def fibonacci (n):
 	if n == 1 or n == 2:
 		return 1
 	return fibonacci(n-1)+fibonacci(n-2)
-suma = 0
-for fibonacci in range (0, 4000000):
-	if fibonacci%2==0:
-		suma= fibonacci + suma
 
-print suma
+suma = 0
+n = 0 
+while True:
+	n = n+1
+	x= fibonacci(n)
+	if x<4000000:
+		if x%2==0:
+			suma= x + suma
+			print suma
+	else:
+		break
+print "end"
