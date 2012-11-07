c68837832de6593f8a2073223c41248d905ea514
Anastasia Coda <nastiuka@gmail.com>
2012-11-07 20:24:14 +0200

    PE3

--- helloWorld/Euler/PE 3.py
+++ helloWorld/Euler/PE 3.py
@@ -1,8 +1,41 @@
-def nump(n):
+# number = raw_input('Enter number:')
+# print "number is", number
+
+def is_prim(n):
 	for x in range (2,n):
-		if n%x==0:
+		if n % x == 0:
 			return False
 	return True
-number = raw_input ('Enter number:')
-if number%nump(n)==0:
-	print nump(n)
+
+
+def isprime(n):
+    '''check if integer n is a prime'''
+    # make sure n is a positive integer
+    n = abs(int(n))
+    # 0 and 1 are not primes
+    if n < 2:
+        return False
+    # 2 is the only even prime number
+    if n == 2: 
+        return True    
+    # all other even numbers are not primes
+    if not n & 1: 
+        return False
+    # range starts with 3 and only needs to go up the squareroot of n
+    # for all odd numbers
+    for x in range(3, int(n**0.5)+1, 2):
+        if n % x == 0:
+            return False
+    return True
+
+number = 600851475143
+#max(nump(n))<number
+n=2
+
+while n<number:
+	n+=1
+	if isprime(n):
+		if number%n==0:
+			print n
+
+print "end"
---
+++ helloWorld/try.py
@@ -0,0 +1,1 @@
+number = raw_input("Enter number:")
